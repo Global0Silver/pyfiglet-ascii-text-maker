97874f26d57388a92ca654d29e09e74771042eb3
Global silver <87803221+Global0Silver@users.noreply.github.com>
2022-04-28 11:19:32 +0300

    Update ascii_gen.py
    fixed bugged

--- ascii_gen.py
+++ ascii_gen.py
@@ -25,45 +25,43 @@ print(Colorate.Vertical(Colors.blue_to_purple, intro, 1))
 print(Colors.white)
 asci = input('Enter your text:')
 fontus = input('Enter font:')
-colors = input ("do u want the text to be coloured?y/n:")
-if colors == ("y"):
- print ("Colour list:")
- print(Colorate.Color(Colors.red, "Red", True))
- print(Colorate.Color(Colors.orange, "Orange", True))
- print(Colorate.Color(Colors.yellow, "yellow", True)) 
- print(Colorate.Color(Colors.green, "Green", True))
- print(Colorate.Color(Colors.blue, "blue", True))
- print(Colorate.Color(Colors.purple, "purple", True))
- print(Colorate.Color(Colors.pink, "pink", True))
- print ("leave blank if none")
- colour = input('Enter colour:')
+
+print ("Colour list:")
+print(Colorate.Color(Colors.red, "Red", True))
+print(Colorate.Color(Colors.orange, "Orange", True))
+print(Colorate.Color(Colors.yellow, "yellow", True)) 
+print(Colorate.Color(Colors.green, "Green", True))
+print(Colorate.Color(Colors.blue, "blue", True))
+print(Colorate.Color(Colors.purple, "purple", True))
+print(Colorate.Color(Colors.pink, "pink", True))
+print ("leave blank if none")
+colour = input('Enter colour:')
+
 if colour == "Red" or colour == "red":
  print(Colors.red)
-elif colour == "Orange" or colour == "orange":
- print(Colors.orange)
-elif colour == "Yellow" or colour == "yellow":
- print(Colors.yellow)
-elif colour == "Green" or colour == "green":
- print(Colors.green)
-elif colour == "Blue" or colour == "blue":
- print(Colors.blue)
-elif colour == "Purple" or colour == "purple":
- print(Colors.purple)
-elif colour == "Pink" or colour == "pink":
- print(Colors.pink)
-elif colour == 0 :
- print ("")
-else:
-  print(Colorate.Error(Colors.red, "that colour doesnt exist check the capitalization", True)) #fix this
-  print ("Restarting...")
-  time.sleep(1)
-  os.system("python ascii_gen.py")
-  clear
+ if colour == "Orange" or colour == "orange":
+  print(Colors.orange)
+ elif colour == "Yellow" or colour == "yellow":
+  print(Colors.yellow)
+ elif colour == "Green" or colour == "green":
+  print(Colors.green)
+ elif colour == "Blue" or colour == "blue":
+  print(Colors.blue)
+ elif colour == "Purple" or colour == "purple":
+  print(Colors.purple)
+ elif colour == "Pink" or colour == "pink":
+  print(Colors.pink)
+#else:
+ # print(Colorate.Error(Colors.red, "that colour doesnt exist check the capitalization", True)) #fix this
+  #print ("Restarting...")
+  #time.sleep(1)
+  #os.system("python w.py")
   
+
 if(len(fontus) == 0):
   fontus = ('roman')
 
-text =print(pyfiglet.figlet_format(asci ,font = fontus ))
+text =print (pyfiglet.figlet_format(asci ,font = fontus ))
 print(Colors.white)
 
 input ("")
